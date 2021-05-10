a96e6cdc2e7843309ab45998a1328c2afe96aabe
Chris Lynch <chris.lynch@hpe.com>
2021-05-10 10:37:27 -0700

    - Initial HPE OneView 6.10 library release. - Deprecated Invoke-OVVcmMigration as the functionality was removed from HPE OneView 5.40. - [#564] Fixed Set-OVSmtpConfig to send a properly formatted request to the appliance    REST API to disable email alert filters. - Added -UseMXRecordLookup to Set-OVSmtpConfig Cmdlet to allow for DNS MX record lookup to be used instead of specifying an SMTP server with the -Server parameter. - Enhanced Get-OVServer to return expanded server hardware inventory. This now fully populates the subResources property with available inventory data. - [#5] Fixed Set-OVUplinkSet to add and remove associated networks when adding/removing network set(s). - Added Windows Server 2019 and Ubuntu values for HostOStype parameter within New-OVServerProfile, New-OVServerProfileTemplate and New-OVServerProfileAttachVolume Cmdlets. - [#569] Refactored detection of duplicate JSON pattern in API response, mainly with FW baselines/SPPs.
    Signed-off-by: Chris Lynch <chris.lynch@hpe.com>

---
+++ Samples/DefineLogicalInterconnectGroup_Sample.ps1
@@ -0,0 +1,73 @@
+﻿##############################################################################
+# DefineLogicalInterconnectGroup_Sample.ps1
+# - Example script for creating Logical Interconnect Groups.
+#
+#   VERSION 1.0
+#
+# (C) Copyright 2013-2021 Hewlett Packard Enterprise Development LP
+##############################################################################
+<#
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+#>
+##############################################################################
+if (-not (get-module HPEOneView.540))
+{
+
+    Import-Module HPEOneView.540
+
+}
+
+if (-not $ConnectedSessions)
+{
+
+	$Appliance = Read-Host 'ApplianceName'
+	$Username  = Read-Host 'Username'
+	$Password  = Read-Host 'Password' -AsSecureString
+
+    $ApplianceConnection = Connect-OVMgmt -Hostname $Appliance -Username $Username -Password $Password
+
+}
+
+#Verify some Ethernet Networks exist
+$networks = Get-OVNetwork -type Ethernet
+
+if ($networks -eq $null)
+{
+
+    write-host "There are no defined Ethernet Networks. Please create some.";
+
+    break
+
+}
+
+$LIGName = "LIG Prod"
+$Bays = @{1 = "FlexFabric";2 = "FlexFabric"}
+$SNMP = @{readCommunity = "MyTr@p1"; enabled=$True; systemContact = "Network Admin"; snmpAccess = @("192.168.1.2/32","10.1.1.0/24");trapDestinations = @(@{trapDestination="myhost.local";communityString="MyTr@p2";trapFormat="SNMPv1";trapSeverities=@("Critical", "Major", "Minor", "Warning", "Normal", "Info", "Unknown");fcTrapCategories=@("PortStatus", "Other")})}
+
+$NewLig = New-OVLogicalInterconnectGroup -Name $LIGName -bays $bays -snmp $snmp | Wait-OVTaskComplete | Get-OVLogicalInterconnectGroup
+
+#Create an Ethernet Uplink Set
+$Networks = "red","blue","green" | Get-OVNetwork -Type Ethernet
+$FabricA = Get-OVNetwork -Name "Production Fabric A" -Type FibreChannel
+$FabricB = Get-OVNetwork -Name "Production Fabric B" -Type FibreChannel
+$newUT = $NewLig | New-OVUplinkSet -Name LUT1 -Type "Ethernet" -Networks $Networks -NativeEthNetwork $Networks[0] -UplinkPorts "BAY1:X4","BAY1:X5","BAY2:X4","BAY2:X5" -EthMode "Auto"
+$NewLig = Get-OVLogicalInterconnectGroup -Name $LIGName
+$newUT = $NewLig | New-OVUplinkSet -Name "Fabric A" -Type "FibreChannel" -Networks $FabricA -UplinkPorts "BAY1:X1,BAY1:X2"
+$NewLig = Get-OVLogicalInterconnectGroup -Name $LIGName
+$newUT = $NewLig | New-OVUplinkSet -Name "Fabric B" -Type "FibreChannel" -Networks $FabricB -UplinkPorts "BAY2:X1,BAY2:X2"
---
+++ Samples/Update-UserScript.ps1
@@ -0,0 +1,90 @@
+##############################################################################
+# Update-UserScript.ps1
+#
+# This script can be used independently from the HPEOneView.540 library to
+# parse a user script and update all legacy HPEOneView Cmdlet names from
+# {VERB}-OV{Nound} to {VERB}-OV{NOUN}, and any reference to HPOneView to
+# HPEOneView.
+#
+#  -Path is a paramter that will iterate through the provided script file.
+#
+#  -Replace is used to commit the changes.  By default, this script will display
+#           only the changes it will make, and not save them.  Use this parameter
+#           to save the results.
+#
+#   VERSION 1.0
+#
+# (C) Copyright 2013-2021 Hewlett Packard Enterprise Development LP
+##############################################################################
+<#
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+#>
+##############################################################################
+
+[CmdletBinding ()]
+Param
+(
+
+    [Parameter (Mandatory, ValueFromPipeline)]
+    [ValidateNotNullOrEmpty()]
+    [System.IO.FileInfo]$Path,
+
+    [Parameter (Mandatory = $false)]
+    [Switch]$Replace
+
+)
+
+Process
+{
+
+    $CmdletSearchExpression   = [System.Text.RegularExpressions.Regex]::new("(?'CmdletVerb'add|copy|get|new|remove|set|show|wait|clear|connect|convert|convertto|disable|disconnect|enable|enter|exit|import|install|invoke|join|ping|pop|push|reset|restart|restore|save|search|send|start|stop|test|update)-(?'VendorPrefix'hpov)(?'CmdletNoun'[a-z]+)", [System.Text.RegularExpressions.RegexOptions]::IgnoreCase)
+    $OtherBrandStyleSearchExp = [System.Text.RegularExpressions.Regex]::new("(?'Libraryname'HPEOneView\.)", [System.Text.RegularExpressions.RegexOptions]::IgnoreCase)
+    $StringBuilder            = [System.Text.StringBuilder]::new()
+    $File                     = [System.IO.StreamReader]::new((Resolve-Path $Path))
+    $ReplacementString        = '${CmdletVerb}-OV${CmdletNoun}'
+    $LibraryReplacementString = 'HPEOneView.'
+
+    While (-not ($file.EndOfStream))
+    {
+        $line = $file.ReadLine()
+
+        $UpdatedLine = $CmdletSearchExpression.Replace($line, $ReplacementString)
+        $UpdatedLine = $OtherBrandStyleSearchExp.Replace($UpdatedLine, $LibraryReplacementString)
+
+        [void]$StringBuilder.Append(($UpdatedLine + "`n"))
+
+    }
+
+    $file.Close()
+
+    if ($PSBoundParameters.Keys -Contains 'Replace')
+    {
+
+        [System.IO.File]::WriteAllLines((Resolve-Path $Path), $StringBuilder.ToString())
+
+    }
+
+    else
+    {
+
+        $StringBuilder
+
+    }
+
+}
